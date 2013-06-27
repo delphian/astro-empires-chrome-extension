8fb51fb9e787b24bf5fe0ba2748c495a4996d5d7
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2013-06-27 01:10:39 -0700

    Breaking code into smaller more specific functions.

--- popup.js
+++ popup.js
@@ -1,26 +1,41 @@
 jQuery('document').ready(function ($) {
+  // Populate value from local storage.
+  $('div.credits-value').html(localStorage['credits']);
+
+  // Update values.
   $('button.test').click(function () {
-    var xhr = new XMLHttpRequest();
-    var params = "email="+localStorage['email']+"&pass="+localStorage['password']+"&navigator=Netscape&hostname="+localStorage['server']+"&javascript=false&post_back=false";
-    xhr.onreadystatechange = handleStateChange; // Implemented elsewhere.
-    xhr.open("POST", 'http://juno.astroempires.com/account.aspx', true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.setRequestHeader("Content-length", params.length);
-    xhr.send(params);
+    getAeValue('http://juno.astroempires.com/account.aspx');
   })
 })
 
-function handleStateChange() {
-  if (this.readyState == this.DONE) {
-    if (this.status == 200) {
-      // success!
-      credits = /<a id='credits'.*?([0-9,]+).*?<\/a>/ig.exec(this.responseText);
-      jQuery('div.credits').html('Credits: ' + credits[1]);
-
-      //console.log(this.response);
-      return;
+function getAeValue(url) {
+  var xhr = new XMLHttpRequest();
+  var params = "email="+localStorage['email']+"&pass="+localStorage['password']+"&navigator=Netscape&hostname="+localStorage['server']+"&javascript=false&post_back=false";
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == xhr.DONE) {
+      if (xhr.status == 200) {
+        processAeValue(url, xhr.responseText);
+      }
+      else {
+        alert('Something went wrong.');
+      }
     }
-    // something went wrong
-    console.log(this.status);
   }
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.setRequestHeader("Content-length", params.length);
+  xhr.send(params);   
+}
+
+function processAeValue(url, response) {
+  if (url == 'http://juno.astroempires.com/account.aspx') {
+    credits = /<a id='credits'.*?([0-9,]+).*?<\/a>/ig.exec(response);
+    localStorage['credits'] = credits[1];
+  }
+  // Update the local storage to the ui.
+  printValues();
+}
+
+function printValues() {
+  jQuery('div.credits-value').html(localStorage['credits']);
 }

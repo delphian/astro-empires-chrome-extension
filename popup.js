f03c5e3e610deef531b6835a8dfc93b11680afee
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2013-06-27 23:51:47 -0700

    Adding technology and fleet size.

--- popup.js
+++ popup.js
@@ -3,18 +3,31 @@ jQuery('document').ready(function ($) {
   printValues();
   // Update values when button is clicked.
   $('button.test').click(function () {
-    getAeValue('http://juno.astroempires.com/account.aspx');
+    aeGetData();
   })
 })
 
-function getAeValue(url) {
-  xhr = new XMLHttpRequest();
-  var params = "email="+localStorage['email']+"&pass="+localStorage['password']+"&navigator=Netscape&hostname="+localStorage['server']+"&javascript=false&post_back=false";
+function ajaxRequest(url, type, params) {
+  var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = ajaxResponse;
-  xhr.open("POST", url, true);
-  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  xhr.setRequestHeader("Content-length", params.length);
-  xhr.send(params);   
+  xhr.open(type, url, true);
+  // No post parameters exist, this may be a simple GET.
+  if (null === params) {
+    xhr.send();
+  }
+  else {
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send(params);
+  }
+} 
+
+function aeLogin() {
+  var params = "email="+localStorage['email']+"&pass="+localStorage['password']+"&navigator=Netscape&hostname="+localStorage['server']+"&javascript=false&post_back=false";
+  ajaxRequest('http://juno.astroempires.com/login.aspx', 'POST', params);
+}
+
+function aeGetData() {
+  ajaxRequest('http://juno.astroempires.com/account.aspx', 'GET');
 }
 
 function ajaxResponse() {
@@ -24,6 +37,8 @@ function ajaxResponse() {
       // Check if we have been asked to login.
       if (this.responseText.match(/<title>Login.*?<\/title>/i)) {
         console.log('Login required');
+        aeLogin();
+        aeGetData();
       }
       else {
         processAeValue(this.responseText);
@@ -42,6 +57,12 @@ function processAeValue(response) {
   if ((income = /<td>[\s]*<b>Empire Income<\/b>[\s]*<\/td>[\s]*<td>([0-9,]+)/ig.exec(response)) && (income.length > 0)) {
     localStorage['income'] = income[1];
   }
+  if ((fleetSize = /<td>[\s]*<b>Fleet Size<\/b>[\s]*<\/td>[\s]*<td>([0-9,]+)/ig.exec(response)) && (fleetSize.length > 0)) {
+    localStorage['fleetSize'] = fleetSize[1];
+  }
+  if ((technology = /<td>[\s]*<b>Technology<\/b>[\s]*<\/td>[\s]*<td>([0-9,]+)/ig.exec(response)) && (technology.length > 0)) {
+    localStorage['technology'] = technology[1];
+  }
   // Update the local storage to the ui.
   printValues();
 }
@@ -49,4 +70,6 @@ function processAeValue(response) {
 function printValues() {
   jQuery('div.credits-value').html(localStorage['credits']);
   jQuery('div.income-value').html(localStorage['income'] + ' per hour');
+  jQuery('div.fleetsize-value').html(localStorage['fleetSize']);
+  jQuery('div.technology-value').html(localStorage['technology']);
 }

91c375e85b3c2e96a5a7dd9ed9e09afc72e47c60
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2013-06-27 22:41:33 -0700

    Checking if we are logged in or not.

--- popup.js
+++ popup.js
@@ -8,29 +8,38 @@ jQuery('document').ready(function ($) {
 })
 
 function getAeValue(url) {
-  var xhr = new XMLHttpRequest();
+  xhr = new XMLHttpRequest();
   var params = "email="+localStorage['email']+"&pass="+localStorage['password']+"&navigator=Netscape&hostname="+localStorage['server']+"&javascript=false&post_back=false";
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState == xhr.DONE) {
-      if (xhr.status == 200) {
-        processAeValue(url, xhr.responseText);
-      }
-      else {
-        alert('Something went wrong.');
-      }
-    }
-  }
+  xhr.onreadystatechange = ajaxResponse;
   xhr.open("POST", url, true);
   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
   xhr.setRequestHeader("Content-length", params.length);
   xhr.send(params);   
 }
 
-function processAeValue(url, response) {
-  if (url == 'http://juno.astroempires.com/account.aspx') {
-    credits = /<a id='credits'.*?([0-9,]+).*?<\/a>/ig.exec(response);
-    income = /<td>[\s]*<b>Empire Income<\/b>[\s]*<\/td>[\s]*<td>([0-9,]+)/ig.exec(response);
+function ajaxResponse() {
+  if (this.readyState == this.DONE) {
+    // Everything went ok.
+    if (this.status == 200) {
+      // Check if we have been asked to login.
+      if (this.responseText.match(/<title>Login.*?<\/title>/i)) {
+        console.log('Login required');
+      }
+      else {
+        processAeValue(this.responseText);
+      }
+    }
+    else {
+      console.log('Status: ' + this.status);
+    }
+  }  
+}
+
+function processAeValue(response) {
+  if ((credits = /<a id='credits'.*?([0-9,]+).*?<\/a>/ig.exec(response)) && (credits.length > 0)) {
     localStorage['credits'] = credits[1];
+  }
+  if ((income = /<td>[\s]*<b>Empire Income<\/b>[\s]*<\/td>[\s]*<td>([0-9,]+)/ig.exec(response)) && (income.length > 0)) {
     localStorage['income'] = income[1];
   }
   // Update the local storage to the ui.

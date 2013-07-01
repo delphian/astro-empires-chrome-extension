393abac1ee996b6e7ab4f03a07d21e88c42f4ee7
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2013-07-01 15:37:40 -0700

    Using new namespaced format.

--- popup.js
+++ popup.js
@@ -1,29 +1,29 @@
 
-var astroEmpires = new AEObject(localStorage['server'], localStorage['email'], localStorage['password']);
+var ae = new AstroEmpires.AE(localStorage['server'], localStorage['email'], localStorage['password']);
 
 function get_credits(data, messageType, observed, response) {
     if ((credits = /<a id='credits'.*?([0-9,]+).*?<\/a>/ig.exec(data)) && (credits.length > 0)) {
         return credits[1];
     }
 }
-astroEmpires.subscribe('get_credits', get_credits);
+ae.subscribe('get_credits', get_credits);
 
 jQuery('document').ready(function ($) {
     // Populate values from local storage.
-    printValues(astroEmpires);
+    printValues(ae);
     // Update values when button is clicked.
     $('button.test').click(function () {
-        astroEmpires.aeGetData();
+        ae.getData();
         // @todo This is always going to be 1 click behind.
-        printValues(astroEmpires);
+        printValues(ae);
     });
 });
 
 function printValues(ae) {
-    jQuery('div.credits-value').html(ae.aeStats.credits);
-    jQuery('div.income-value').html(ae.aeStats.income + ' per hour');
-    jQuery('div.fleetsize-value').html(ae.aeStats.fleetSize);
-    jQuery('div.technology-value').html(ae.aeStats.technology);
-    jQuery('div.level-value').html(ae.aeStats.level);
-    jQuery('div.rank-value').html(ae.aeStats.rank);
+    jQuery('div.credits-value').html(ae.stats.credits);
+    jQuery('div.income-value').html(ae.stats.income + ' per hour');
+    jQuery('div.fleetsize-value').html(ae.stats.fleetSize);
+    jQuery('div.technology-value').html(ae.stats.technology);
+    jQuery('div.level-value').html(ae.stats.level);
+    jQuery('div.rank-value').html(ae.stats.rank);
 }

0faa0aa052e24773143ec654e0a17a044274ac89
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2013-07-05 02:14:19 -0700

    html5 notifications working for guild messages.

---
+++ background.js
@@ -0,0 +1,55 @@
+
+var ae = new AstroEmpires.AE(localStorage['server'], localStorage['email'], localStorage['password']);
+
+/**
+ * Return a reference to the popup window object.
+ *
+ * @return object|bool
+ *   The window object if it exists (open), false otherwise.
+ */
+function getPopup() {
+    var popup = false,
+        popups = chrome.extension.getViews({type: "popup"});
+    if (popups.length > 0) {
+        popup = popups[0];
+    }
+    return popup;
+}
+
+jQuery('document').ready(function ($) {
+    // Populate values from local storage.
+    ae.getData();
+    $('')
+    // Poll for updates.
+    setInterval(function () {
+        ae.getData();
+        printValues(ae);
+    }, 10000);
+});
+
+function printValues(ae) {
+    if (popup = getPopup()) {
+        popup.printValues(ae);
+    }
+}
+
+/**
+ * Update popup with new messages received.
+ */
+function printMessage(data, messageType, ae) {
+    if (popup = getPopup()) {
+        popup.printMessage(data, messageType, ae);
+    }
+    window.webkitNotifications.createNotification('icon.png', 'Astro Empires Message', data.message.message).show();
+}
+ae.subscribe('msg_add_pre', printMessage);
+
+/**
+ * Updating the popup with what we are currently doing.
+ */
+function printStatus(data, messageType, ae) {
+    if (popup = getPopup()) {
+        popup.printStatus(data, messageType, ae);
+    }
+}
+ae.subscribe('ajax', printStatus);
--- popup.js
+++ popup.js
@@ -1,16 +1,4 @@
 
-var ae = new AstroEmpires.AE(localStorage['server'], localStorage['email'], localStorage['password']);
-
-jQuery('document').ready(function ($) {
-    // Populate values from local storage.
-    ae.getData();
-    // Poll for updates.
-    setInterval(function () {
-        ae.getData();
-        printValues(ae);
-    }, 5000);
-});
-
 function printValues(ae) {
     jQuery('div.credits-value').html(ae.stats.credits);
     jQuery('div.income-value').html(ae.stats.income + ' per hour');
@@ -28,7 +16,6 @@ function printMessage(data, messageType, ae) {
     chrome.browserAction.setTitle({title: data.message.message.replace(/<(?:.|\n)*?>/gm, '')});
     message.html(data.message.message);
 }
-ae.subscribe('msg_add_pre', printMessage);
 
 /**
  * Updating the popup with what we are currently doing.
@@ -39,4 +26,3 @@ function printStatus(data, messageType, ae) {
     var status = jQuery('div.ae-status-container div.content');
     status.html(n);
 }
-ae.subscribe('ajax', printStatus);

04a66ad8c3c97f21a1daa450ea251ea567773c90
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2013-07-06 23:56:58 -0700

    Fixing deploy script to pull out undesired .git and installation scripts.

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Copy the existing directory structure, cleaning out any git references, then zips the result.
+
+echo "This will copy this entire directory tree, less git related files, into a new tree, and zip up for distribution as a chrome extension."
+echo ""
+echo "Do you wish to continue? (y/n) "
+read INPUT
+if [ "$INPUT" == 'y' ]; then
+  N=$RANDOM$RANDOM
+  # Create a new directory in tmp to hold our distribution.
+  mkdir "/tmp/$N"
+  # Copy everything into the new directory.
+  cp -r . "/tmp/$N"
+  # Remove all references to git.
+  find "/tmp/$N" -name ".git*" -exec rm -Rf {} \;
+  # Remove other files.
+  rm "/tmp/$N/deploy.sh"
+  rm "/tmp/$N/install.sh"
+  tar -zcvf astro-empires-observer.tar.gz "/tmp/$N"
+fi
